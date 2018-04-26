2889c21430ee1f44131a5852f7e846831e7e2148
Chander <rc250348@ncr.com>
2018-04-26 11:00:31 +0530

    updated solution arch

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return(
+    <footer>
+      <nav>
+        <ul>
+          <li>About Us</li>
+          <li>Trainings</li>
+          <li>Team</li>
+          <li>Testimonial</li>
+          <li>Training</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return(
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Team</li>
+          <li>Testimonial</li>
+          <li>Training</li>
+          <li>Contact Us</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
