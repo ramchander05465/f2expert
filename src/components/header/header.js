ba35a0cdbc1e9efe4915aea9b3cd7a1d1f97c3fa
Chander <rc250348@ncr.com>
2018-05-09 10:51:40 +0530

    included images

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,48 @@
 import React from 'react'
+import { 
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem   } from 'reactstrap';
 
-const Header = () => {
+//import './header.css'
+
+const Header = (props) => {  
   return(
-    <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Team</li>
-          <li>Testimonial</li>
-          <li>Training</li>
-          <li>Contact Us</li>
-          <li>Login</li>
-        </ul>
-      </nav>
+    <header className="header-top">
+      <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">F2Expert</NavbarBrand>
+          <NavbarToggler onClick={props.toggleNavbar} />
+          <Collapse isOpen={props.collapsed} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Home</NavLink>
+              </NavItem>              
+              <NavItem>
+                <NavLink href="/components/">About Us</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/">Team</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/">Testimonials</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/">Trainings</NavLink>
+              </NavItem>
+              <NavItem> 
+                <NavLink href="/components/">Contact Us</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
     </header>
   )
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
 
 class App extends Component {
+  state = {
+    collapsed: true
+  }
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header toggleNavbar = {this.toggleNavbar} collapsed = {this.state.collapsed} />
+        <Footer />
       </div>
     );
   }
